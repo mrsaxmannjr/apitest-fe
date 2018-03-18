8b0ac35269de9f6faf87fccf750a8a541b623675
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-03-18 15:33:30 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a className="navbar-brand" href="#">Test</a>
+          </nav>
+        </header>
+        <main className="container">
+          <h2>Test</h2>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
